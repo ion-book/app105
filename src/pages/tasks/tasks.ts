b15430bfac29b359e450ec1450d9d28435eb8969
Nicolas Molina Monroy <nicolas.molina.monroy@gmail.com>
2017-03-30 21:46:49 -0500

    get and create

--- src/pages/tasks/tasks.ts
+++ src/pages/tasks/tasks.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, AlertController } from 'ionic-angular';
 
 import { TasksService } from '../../providers/tasks-service';
 
@@ -15,11 +15,56 @@ export class TasksPage {
   constructor(
     public navCtrl: NavController, 
     public navParams: NavParams,
-    public tasksService: TasksService
+    public tasksService: TasksService,
+    public alertCtrl: AlertController
   ) {}
 
   ionViewDidLoad() {
-    this.tasks = this.tasksService.getAll();
+    this.tasksService.getAll('8')
+    .then((tasks: any) =>{
+      this.tasks = tasks;
+    })
+    .catch(error =>{
+      console.error(error);
+    });
+  }
+
+  createTask(){
+    let prompt = this.alertCtrl.create({
+      title: 'Nueva tarea',
+      message: "Digite la nueva tarea",
+      inputs: [
+        {
+          name: 'title',
+          placeholder: 'Title'
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Save',
+          handler: data => {
+            let newTask ={
+              title: data.title,
+              completed: false
+            }
+            this.tasksService.create( newTask )
+            .then(resultTask =>{
+              this.tasks.unshift( resultTask );
+            })
+            .catch(error =>{
+              console.error( error );
+            });
+          }
+        }
+      ]
+    });
+    prompt.present();
   }
 
 }
